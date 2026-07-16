7a8311e1a7877c2ebb21dfeeeec2eb344980bd6a
luqman-main <mluqmakhan@gmail.com>
2026-07-16 14:52:35 +0500

    Add JWT token to response as a cookie
    Set cookie with JWT token for user session management.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -76,6 +76,10 @@ const verifyOTP = async (req, res) => {
         const token = jwt.sign({ id: user._id ,
             account: account._id
         }, process.env.JWT_SECRET, { expiresIn: '1d' });
+           res.cookie("token", token, {
+            httpOnly: true,
+            maxAge: 24 * 60 * 60 * 1000,
+        });
         res.status(200).json({ message: 'OTP verified successfully', token });
     } catch (error) {
         res.status(500).json({ error: error.message });
